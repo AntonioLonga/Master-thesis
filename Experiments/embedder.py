d653dd3541eb78897b370caa701cd2c6649cb4b9
Antonio Longa <longaantonio@gmail.com>
2019-03-25 11:16:28 +0000

    refactoring on class evaluator, model and embedder)

--- Experiments/embedder.py
+++ Experiments/embedder.py
@@ -1,55 +1,56 @@
-from eden.graph import vectorize
-import numpy as np
-
-
-
-class embedder:
-    model_high_to_medium = None
-    model_medium_to_small = None
-    """ Constructor that take in input two models"""
-    def __init__(self, model_medium_to_small, model_high_to_medium="vectorize"):
-        """ Constructor that take in input two models:
-            model_high_to_medium -> first reduction from really high space to a medium one"""
-        # instanziate first model
-        # Default = vectorize
-        embedder.model_medium_to_small = model_medium_to_small
-        # instanziate second model
-        embedder.model_high_to_medium = model_high_to_medium
+class Embedder:
+    estimator_graph_to_high = None
+    estimator_high_to_medium = None
+    estimator_medium_to_small = None
+    n_estimator = 2
+    
+    def __init__(self, graph_to_high, high_to_medium, medium_to_small=None):
+        self.estimator_graph_to_high = Model(graph_to_high)
+        self.estimator_high_to_medium = Model(high_to_medium)
+        self.estimator_medium_to_small = Model(medium_to_small)
+        if (medium_to_small != None):
+            self.n_estimator = 3
         
-    def fit(self, graphs):
-        # fit the first model
-        X = self.__fit_high_to_medium(graphs)
-        # fit the second model
-        self.__fit_medium_to_small(X)
+    def fit(self,X,y):
         
-    def transform(self, graphs):
-        # use the first model to predict data
-        Xd = self.__transform_high_to_medium(graphs)
-        # use the second modle to predict data coming from first model
-        y_pred = self.__transform_medium_to_small(Xd)
+        if (self.n_estimator == 3):
+            X_high = self.estimator_graph_to_high.fit(X,y).transform(X)
+            X_medium = self.estimator_high_to_medium.fit(X_high,y).transform(X_high)
+            X_small = self.estimator_medium_to_small.fit(X_medium,y)
+        else:
+            X_high = self.estimator_graph_to_high.fit(X,y).transform(X)
+            X_medium = self.estimator_high_to_medium.fit(X_high,y)
+        
+        return(self)
+    
+    def transform(self,X):
+        
+        if (self.n_estimator == 3):
+            X_high = self.estimator_graph_to_high.transform(X)
+            X_medium = self.estimator_high_to_medium.transform(X_high)
+            X_small = self.estimator_medium_to_small.transform(X_medium)
+        else:
+            X_high = self.estimator_graph_to_high.transform(X)
+            X_small = self.estimator_high_to_medium.transform(X_high)
+        
+        return(X_small)
+        
+
+class Model:
+    estimator = None
+    has_fit = True
+    
+    def __init__(self, estimator, has_fit=True):
+        self.has_fit = has_fit
+        self.estimator = estimator
+    
+    def fit(self,X,y):
+        if (self.has_fit == True):
+            self.estimator.fit(X,y)
+        return(self)
+    
+    def transform(self,X):
+
+        y_pred = self.estimator.transform(X)
         return(y_pred)
-    
-    
-    def __fit_high_to_medium(self, graphs):
-        if (embedder.model_high_to_medium == "vectorize"):
-            X = vectorize(graphs, complexity=12)
-        else:
-            ###### something like
-            # X = model_high_to_medium.fit(graphs)
-            X = None
-        return (X)
-    
-    def __transform_high_to_medium(self, graphs):
-        if (embedder.model_high_to_medium == "vectorize"):
-            X = vectorize(graphs, complexity=12, )
-        else:
-            ###### something like
-            # X = model_high_to_medium.transform(graphs)
-            X = None
-        return (X)
-    
-    def __fit_medium_to_small(self, X):
-        embedder.model_medium_to_small.fit(X)
         
-    def __transform_medium_to_small(self, X):
-        return(embedder.model_medium_to_small.transform(X))
--- Experiments/evaluator.py
+++ Experiments/evaluator.py
@@ -3,35 +3,38 @@ from sklearn import metrics
 from sklearn.model_selection import cross_val_score
 import numpy as np
 
-class evaluator:
+class Evaluator:
+    '''
+    This class use K neighbours Classifier to valuate an embedding. 
+    It has a construcctor, that thakes in input the number of neighbour, and
+    a method that compute accuracy percision recall and f1 score using 
+    n_folds cross validation.
+    '''
     classifier = None
     n_neighbors = 0
     
     def __init__(self,n_neighbors=1):        
         self.classifier = KNeighborsClassifier(n_neighbors=n_neighbors)
         self.n_neighbors = n_neighbors
-    
-    def fit(self,X,y):
-        self.classifier.fit(X,y)
-        
-    def evaluate(self, X ,y_real):
-        y_pred = []
-        
-        
-        for i in X:
-            y_pred.append(self.classifier.predict([i]))
-        y_pred = np.array(y_pred)
-        
-        acc = metrics.accuracy_score(y_pred,y_real)
-        pre = metrics.precision_score(y_pred,y_real)
-        rec = metrics.recall_score(y_pred,y_real)
-        f1 = metrics.f1_score(y_pred,y_real)
-        
-        return([acc,pre,rec,f1])
 
-    def evaluate_with_kfold(self,X,y,n_folds=10):
+
+    def performance_with_kfold(self,X,y,n_folds=10):
+        '''
+        It takes in input a set X and it's label and the number of folds (default = 10)
+        It return an array of lists [accuracy, precision, recall, f1] 
+        length n_folds.
+        '''
+
         classif = KNeighborsClassifier(n_neighbors = self.n_neighbors)
-        score_train = cross_val_score(classif ,X,y, cv=n_folds)
         
-        return(np.mean(score_train),np.std(score_train))
+        accuracy_train = list(cross_val_score(classif ,X,y, cv=n_folds, 
+                                         scoring = metrics.make_scorer(metrics.accuracy_score)))
+        precision_train = list(cross_val_score(classif ,X,y, cv=n_folds, 
+                                          scoring = metrics.make_scorer(metrics.precision_score)))
+        recall_train = list(cross_val_score(classif ,X,y, cv=n_folds, 
+                                       scoring = metrics.make_scorer(metrics.recall_score)))
+        f1_train = list(cross_val_score(classif ,X,y, cv=n_folds, 
+                                   scoring = metrics.make_scorer(metrics.f1_score)))
+        
+        return([accuracy_train,precision_train,recall_train,f1_train])
         
